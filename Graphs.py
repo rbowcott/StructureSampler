9a2912f9bca227a92dfb423dbad0cf6775547996
Redmond Bowcott <s2157200@ed.ac.uk>
2025-02-20 12:17:45 +0000

    Need to fix data storage

--- Graphs.py
+++ Graphs.py
@@ -1,6 +1,5 @@
 import networkx as nx
 import pickle
-import matplotlib.pyplot as plt
 from collections import Counter
 
 with open('out.pkl', 'rb') as f:
@@ -10,4 +9,5 @@ n = 1
 state_counts = Counter(all_visited)
 top_n_states = state_counts.most_common(n)
 
-# G = nx.from_numpy_array(top_n_states[0], create_using=nx.DiGraph)
+for state, count in top_n_states:
+    G = nx.from_numpy_array(state, create_using=nx.DiGraph)
